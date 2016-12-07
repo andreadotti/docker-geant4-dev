8446cd9486ed8b441e9fc1df1bd2dff3b40ee735
andreadotti <adotti@slac.stanford.edu>
2016-12-07 15:23:54 -0800

    Improving buildme script
    Number of parallel build is not fixed anymore, and exit immediately in case
    of errors

--- buildme.sh
+++ buildme.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-
+set -e
 cmake -DGEANT4_INSTALL_DATA=ON \
       -DGEANT4_INSTALL_DATADIR=/usr/local/geant4/data \
       -DGEANT4_BUILD_MULTITHREADED=ON \
       -DGEANT4_USE_GDML=ON \
       -DCMAKE_INSTALL_PREFIX=/usr/local/geant4 \
 	/geant4-src
-make -j24 
+make -j`nproc` 
 make install/fast
 cd /usr/local/geant4
 tar --exclude='./data' -czf /build/binaries/geant4.tgz .
