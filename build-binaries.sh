e952c023c52c2f815a54423da77c18716a4d93f0
andreadotti <adotti@slac.stanford.edu>
2016-12-07 17:12:31 -0800

    Fix name issue in build script

--- build-binaries.sh
+++ build-binaries.sh
@@ -6,4 +6,4 @@ fi
 g4src=$1
 g4data=$2
 
-docker run --rm -v "${g4src}:/geant4-src:ro" -v "${g4data}:/usr/local/geant4/data" -v "$PWD:/build" -w "/tmp" andreadotti/geant4-dev-base /build/buildme.sh  
+docker run --rm -v "${g4src}:/geant4-src:ro" -v "${g4data}:/usr/local/geant4/data" -v "$PWD:/build" -w "/tmp" andreadotti/geant4-base-dev /build/buildme.sh  
